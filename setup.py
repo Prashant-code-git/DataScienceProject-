8c85d188ff7db362c7e88a85cb014a19891f8513
Prashant-code-git <prashant.singh081997@gmail.com>
2024-06-08 12:01:49 +0530

    setup commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return  the list of requirements 
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        reqirements= file_obj.readlines()
+        requirements =[req.replace('\n',"")for req in requirements] 
+    return requirements   
+
+setup(
+    name ='DATASCIENCEPROJECT',
+    Version='0.0.1',
+    author= 'Prashant',
+    author_email ='prashant.singh081997@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')    
+)
